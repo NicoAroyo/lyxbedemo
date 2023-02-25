319b0cd7c834ad44ea6e88b24263f7189da42dca
NicoAroyo <nico.aroyo@gmail.com>
2023-02-26 01:13:12 +0200

    fixed typo in jwtHelper

--- auth/middlewares/jwtHelper.js
+++ auth/middlewares/jwtHelper.js
@@ -3,19 +3,21 @@ import env from "dotenv";
 env.config();
 
 export const verifyAccessToken = (req, res, next) => {
-  console.log("hi from");
+  console.log("hi from verifyAccessToken");
   const token = req.headers.authorization.split(" ")[1];
   console.log(token);
   if (!token) {
     return res.status(400).json({ error: "user not authenticated" });
   }
   try {
-    const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
     if (verified) {
       req.user_name = verified.user_name;
+      console.log(verified);
       return next();
     }
   } catch (error) {
+    console.log(error);
     res.status(400).json({ error });
   }
 };
