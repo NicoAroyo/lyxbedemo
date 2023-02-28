c467380e411c62fb3535373dd07791c4c5f88b46
NicoAroyo <nico.aroyo@gmail.com>
2023-02-28 15:17:10 +0200

    rateLimiter small change to be more dynamic

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -1,9 +1,11 @@
 import { rateLimit } from "express-rate-limit";
 
+const amount = 100;
+
 export const rateLimiterUsingThirdParty = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-  max: 100,
-  message: "You have exceeded the 100 requests in 24 hrs limit!",
+  max: amount,
+  message: `You have exceeded the ${amount}requests in 24 hrs limit!`,
   standardHeaders: true,
   legacyHeaders: false,
 });
